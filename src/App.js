58f9c08a7319950a66e8ebd6663b49ec3f70e4a0
Луиза <luiza@MacBook-Air-Luiza.local>
2021-10-26 17:09:47 +0600

    task1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/View.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const View = (props) => {
+    console.log(props.arr)
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default View;
